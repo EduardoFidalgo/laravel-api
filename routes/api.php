2abcff0064d2e40e7624392d46a426366983df63
Eduardo Fidalgo <edu.fidalgo18@gmail.com>
2023-04-30 16:17:52 -0300

    testing

--- routes/api.php
+++ routes/api.php
@@ -23,13 +23,13 @@ Route::get("/", function () {
     return json_encode('Welcome to the Work Out!');
 });
 
+Route::put("/users/{id}", [UserController::class, 'update']);
 Route::post('/login', [LoginController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get("/users", [UserController::class, 'index']);
     Route::get("/users/{id}", [UserController::class, 'show']);
     Route::post("/users", [UserController::class, 'store']);
-    Route::put("/users/{id}", [UserController::class, 'update']);
     Route::delete("/users/{id}", [UserController::class, 'destroy']);
 
     Route::get("/workouts", [WorkoutController::class, 'index']);
